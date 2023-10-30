bd859ffeaab6956c9b4b009a26ba67fa4a2a8067
MirJach <mirjachowicz2001@gmail.com>
2023-10-30 20:28:01 +0100

    Commit 1

--- app/src/main/java/com/example/inzynierka/ClubFragment.java
+++ app/src/main/java/com/example/inzynierka/ClubFragment.java
@@ -1,59 +1,21 @@
 package com.example.inzynierka;
 
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link ClubFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+
 public class ClubFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public ClubFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment ClubFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static ClubFragment newInstance(String param1, String param2) {
-        ClubFragment fragment = new ClubFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
+    private BottomNavigationView club_nav;
+    private NavController navController;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -61,4 +23,49 @@ public class ClubFragment extends Fragment {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_club, container, false);
     }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        club_nav = requireView().findViewById(R.id.club_navigation);
+
+        club_nav.setOnNavigationItemSelectedListener(item -> {
+            int itemId = item.getItemId();
+            if (itemId == R.id.memberFragment) {
+                getChildFragmentManager()
+                        .beginTransaction()
+                        .replace(R.id.fragmentContainerView, new MemberFragment())
+                        .commit();
+                return true;
+            } else if (itemId == R.id.exerciseFragment) {
+                getChildFragmentManager()
+                        .beginTransaction()
+                        .replace(R.id.fragmentContainerView, new ExerciseFragment())
+                        .commit();
+                return true;
+            } else if (itemId == R.id.calendarFragment) {
+                getChildFragmentManager()
+                        .beginTransaction()
+                        .replace(R.id.fragmentContainerView, new CalendarFragment())
+                        .commit();
+                return true;
+            } else if (itemId == R.id.squadFragment) {
+                getChildFragmentManager()
+                        .beginTransaction()
+                        .replace(R.id.fragmentContainerView, new SquadFragment())
+                        .commit();
+                return true;
+            } else if (itemId == R.id.profileFragment) {
+                getChildFragmentManager()
+                        .beginTransaction()
+                        .replace(R.id.fragmentContainerView, new ProfileFragment())
+                        .commit();
+                return true;
+            } else {
+                return false;
+            }
+        });
+    }
+
 }
